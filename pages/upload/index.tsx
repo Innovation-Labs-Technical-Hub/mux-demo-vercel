47200d639f3dafe8141232b5e26260a3192f54f5
Tony Thai <81668387+trungthaihieu93-dev@users.noreply.github.com>
2022-08-17 13:48:47 +0700

    Add Upload Page

---
+++ pages/upload/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function UploadPage() {
+  return (
+    <div>
+      <Head>
+        <title>Upload Page</title>
+      </Head>
+      <div>Upload</div>
+    </div>
+  );
+}
